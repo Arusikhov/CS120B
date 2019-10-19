059f1abacf07ff184c23ed48fe42cf9d4d45854f
Arusyak Hovhannesyan <Harusyak@gmail.com>
2019-10-18 18:43:56 -0700

    final part 1 and 2

---
+++ Lab2-introToAVR/GccApplication1/GccApplication1/CFile2.c
@@ -0,0 +1,6 @@
+/*
+ * CFile2.c
+ *
+ * Created: 10/18/2019 6:24:05 PM
+ *  Author: Suren
+ */ 
--- Lab2-introToAVR/GccApplication1/GccApplication1/main.c
+++ Lab2-introToAVR/GccApplication1/GccApplication1/main.c
@@ -15,16 +15,21 @@
 int main(void) {
     DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
     DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-    unsigned char PAA = 0x00; // Temporary variable to hold the value of A0
-    unsigned char PAB = 0x00; // Temporary variable to hold the value of A1
-    unsigned char PB  = 0x00; // Temporary variable to hold the value of B0
+    DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+    unsigned char PA = 0x00; // Temporary variable to hold the value of A0
+//    unsigned char PA1 = 0x00; // Temporary variable to hold the value of A1
+    unsigned char PB = 0x00; // Temporary variable to hold the value of B0
+    unsigned char cntavail = 0x00;
     while(1) {
         // 1) Read input
-        PAA = PINA & 0x01;
-        PAB = PINA & 0x02;
-        PB  = !PAA & PAB;
-
-        PORTB = PB0;    
+        PA = PINA & 0x0F;
+        //PB0 = !PA0 & PA1;
+        PORTC = cntavail;
+        cntavail = !(PA & 0x01) + !((PA & 0x02)>>1) + !((PA & 0x04)>>2) + !((PA & 0x08)>>3);
+           
+        
+    // 3) Write output
+    PORTB = PB;    
     }
     return 0;
 }
