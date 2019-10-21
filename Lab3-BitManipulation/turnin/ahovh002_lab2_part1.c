2e8679341edd23df2fb076071edf6bb1443acee2
Arusyak Hovhannesyan <Harusyak@gmail.com>
2019-10-20 20:27:05 -0700

    Create ahovh002_lab2_part1.c

---
+++ Lab3-BitManipulation/turnin/ahovh002_lab2_part1.c
@@ -0,0 +1,34 @@
+/*	Author: ahovh002
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif    
+
+int main(void) {
+   DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+   DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs
+   DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
+   unsigned char tmpC = 0x00; // Temporary variable to hold value
+
+    while(1) {
+
+        // shifts all values of PINA and PINB and finds sum
+        tmpC = (PINA & 0x01) + ((PINA & 0x02)>>1) + ((PINA & 0x04)>>2) + ((PINA & 0x08)>>3)
+         + ((PINA & 0x10)>>4) + ((PINA & 0x20)>>5) + ((PINA & 0x40)>>6) + ((PINA & 0x80)>>7)
+         + (PINB & 0x01)     + ((PINB & 0x02)>>1) + ((PINB & 0x04)>>2) + ((PINB & 0x08)>>3)
+         + ((PINB & 0x10)>>4) + ((PINB & 0x20)>>5) + ((PINB & 0x40)>>6) + ((PINB & 0x80)>>7);
+           
+        
+    // output
+    PORTC = tmpC;    
+    }
+    return 0;
+}
