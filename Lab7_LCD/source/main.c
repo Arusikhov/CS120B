6cef13cad6679aaa70b019ce361eaeb934defaa0
Arusyak Hovhannesyan <Harusyak@gmail.com>
2019-11-03 12:00:21 -0800

    Update main.c

--- Lab7_LCD/source/main.c
+++ Lab7_LCD/source/main.c
@@ -3,7 +3,7 @@
 
 int main(void) {
 DDRD = 0xFF; PORTD = 0x00; // LCD data lines
-DDRA = 0xFF; PORTA = 0x00; // LCD control lines
+DDRB = 0xFF; PORTB = 0x00; // LCD control lines
 
 //Initializes the LCD display
 LCD_init();
